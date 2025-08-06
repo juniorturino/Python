b80b1553d9bb10022e1e5888e8990e4649690357
Junior Turino <alvi.turino@gmail.com>
2025-08-06 20:33:36 -0300

    Update variaveis_constantes.py

--- variaveis_constantes.py
+++ variaveis_constantes.py
@@ -8,3 +8,9 @@ limite_saque_diario = 1000
 BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
 
 print(BRAZILIAN_STATES)
+
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python")
