0bb494dc8ce3f1c34b00208150f5c444ac608d70
Junior Turino <alvi.turino@gmail.com>
2025-08-11 19:27:19 -0300

    Treinando Python

---
+++ operadores_aritméticos.py
@@ -0,0 +1,13 @@
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 ** produto_2)
+print(produto_1 % produto_2)
+
+x = 10 + 5 * 4
+print (x)
---
+++ operadores_logicos.py
@@ -0,0 +1,20 @@
+# AND = para ser True, tudo tem que ser True.
+# OR = para ser True, apenas um tem que ser True
+
+print(True and True and True)
+print(True and False and True)
+print(False and False and False)
+print(True or True or True)
+print(True or False or False)
+print(False or False or False)
+
+
+
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+exp = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
