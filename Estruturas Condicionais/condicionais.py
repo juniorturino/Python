eefa9657c25efc2ab15ce401ea126526634675a0
Junior Turino <alvi.turino@gmail.com>
2025-08-12 19:28:58 -0300

    Treinando Python.

---
+++ Estruturas Condicionais/condicionais.py
@@ -0,0 +1,27 @@
+#Começo condicionais
+
+
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
+
+idade = int(input("Informe a sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH")
+
+else:
+    print("Menor de idade ainda não pode tirar a CNH")
+
+
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH")
+
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
+
+else:
+    print("Menor de idade ainda não pode tirar a CNH")
+
+#Fim condicionais
+
